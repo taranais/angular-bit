be643347b4811dd3cf0aabca138922740f68bd88
taranais <taranais@gmail.com>
2017-02-02 13:26:28 +0100

    Update angular-bit.js

--- src/angular-bit.js
+++ src/angular-bit.js
@@ -4,8 +4,24 @@
     // # include "common-utils.js"
 
     angular.module('angular-bit', [
-    ])
+       ])
+           .directive("bitMask", function() {
+               return {
+                   restrict: "A",
+                   require: "ngModel",
+                   link: function($scope, elem, attrs, ngModelCtrl) {
+                       var bitMask = parseInt($scope.$eval(attrs.bitMask));
 
-    ;
+                       ngModelCtrl.$formatters.push(function(mVal) {
+                           return (mVal & bitMask) > 0;
+                       });
+                       ngModelCtrl.$parsers.push(function(vVal) {
+                           var oldValue = ngModelCtrl.$modelValue;
+                           return vVal ? oldValue | bitMask : oldValue & ~bitMask;
+                       });
+                   }
+               };
+           })
+       ;
 
 })();
